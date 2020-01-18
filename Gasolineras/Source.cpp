16999e97452c424639335f2b05b456c08b4152fd
Diego ViVi <dvicentevila@gmail.com>
2020-01-18 12:53:39 +0100

    Codigo comentado

--- Gasolineras/Source.cpp
+++ Gasolineras/Source.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <stdio.h>
-#include <curl/curl.h>
 #include <string>
 #include <fstream>
 
+//Se necesitan instalar las siguientes bibliotecas (curl, json)
+#include <curl/curl.h>
 #include <json/json.h>
 #include <json/reader.h>
 #include <json/writer.h>
@@ -11,8 +12,10 @@
 
 using namespace std;
 
+//Tamaño inicial de la lista
 const int N = 100;
 
+//Estructura tGasolineras, con los datos asociados a una gasolinera en base al tipo de gasolina
 typedef struct{
     string provincia;
     string localidad;
@@ -21,8 +24,10 @@ typedef struct{
     double longitud;
 }tGasolineras;
 
+//Puntero del tipo tGasolineras, que nos servira para generar un array dinamico de objetos del tipo tGasolineras
 typedef tGasolineras* tArrayPunt;
 
+//Estructura tListaGasolineras, nos servira para almacenar el listado dinamico de gasolineras en base a un tipo de gasolina
 typedef struct {
     tArrayPunt registros;
     int contador;
@@ -31,6 +36,7 @@ typedef struct {
 }tListaGasolineras;
 
 
+//Metodos utilizados
 void cargarDatosGasolineras(tListaGasolineras& listaGasolineras, bool &ok);
 static Json::Value ParseJson(string text);
 static size_t WriteCallback(void* contents, size_t size, size_t nmemb, void* userp);
@@ -44,6 +50,7 @@ void menu(tListaGasolineras& listaGasolineras);
 int mostrarMenu();
 void accederLocalizacion(tListaGasolineras listaGasolineras, int posicion);
 
+//Metodo main, inicializara la lista, accederá al menu y por ultimo liberara la memoria una vez el usuario ha terminado de usar el menu.
 int main() {
     tListaGasolineras listaGasolineras;
     menu(listaGasolineras);
@@ -51,11 +58,12 @@ int main() {
     return 0;
 }
 
+//Metodo menu, que nos permitira ejecutar las consultas del usuario
 void menu(tListaGasolineras &listaGasolineras) {
     int op;
     bool ok = false;
     do {
-        op = mostrarMenu();
+        op = mostrarMenu(); //Mostramos el menu
         switch (op) {
             case 1: {
                 int opGasolina;
@@ -66,7 +74,7 @@ void menu(tListaGasolineras &listaGasolineras) {
                 cout << "4. Bioetanol" << endl;
                 cout << "Opcion:";
                 cin >> opGasolina;
-                switch (opGasolina)
+                switch (opGasolina) //Elegimos el tipo de gasolina
                 {
                     case 1: {
                         listaGasolineras.tipoGasolina = "precio_gasolina_95";
@@ -81,8 +89,10 @@ void menu(tListaGasolineras &listaGasolineras) {
                         listaGasolineras.tipoGasolina = "precio_bioetanol";
                     }
                 }
+                //Cargamos los datos en base al tipo de gasolina
                 cerr << "Cargando datos y ordenandolos....Por favor espere. \n";
                 cargarDatosGasolineras(listaGasolineras, ok);
+                //Ordenamos los datos en base al precio de la gasolina
                 merge_sort(listaGasolineras, 0, listaGasolineras.contador - 1);
                 cerr << "Datos cargados y ordenados con exito" << endl;
 
@@ -90,7 +100,9 @@ void menu(tListaGasolineras &listaGasolineras) {
                 system("cls");
             }break;
             case 2: {
+                //Si los datos han sido cargados previamente
                 if (ok) {
+                    //Mostramos todo el listado de gasolineras en base al tipo de gasolina elegido previamente
                     mostrarGasolineras(listaGasolineras);
                 }
                 else {
@@ -101,12 +113,15 @@ void menu(tListaGasolineras &listaGasolineras) {
             }break;
 
             case 3: {
+                //Si los datos han sido cargados previamente
                 if (ok) {
+                    //Mostramos los datos de la gasolinera con el tipo de gasolina mas barato elegido previamente
                     cout << "La gasolina del tipo " << listaGasolineras.tipoGasolina << " mas barata, se encuentra..." << endl;
                     cout << "Localidad: " << listaGasolineras.registros[0].localidad << endl;
                     cout << "Provincia: " << listaGasolineras.registros[0].provincia << endl;
                     cout << "Precio gasolina: " << listaGasolineras.registros[0].precio << endl;
 
+                    //Damos al usuario la opcion de acceder a la localizacion en Google Maps
                     accederLocalizacion(listaGasolineras, 0);
                 }
                 else {
@@ -117,12 +132,15 @@ void menu(tListaGasolineras &listaGasolineras) {
             }break;
 
             case 4: {
+                //Si los datos han sido cargados previamente
                 if (ok) {
+                    //Mostramos los datos de la gasolinera con el tipo de gasolina mas caro elegido previamente
                     cout << "La gasolina del tipo " << listaGasolineras.tipoGasolina << " mas barata, se encuentra..." << endl;
                     cout << "Localidad: " << listaGasolineras.registros[listaGasolineras.contador-1].localidad << endl;
                     cout << "Provincia: " << listaGasolineras.registros[listaGasolineras.contador-1].provincia << endl;
                     cout << "Precio gasolina: " << listaGasolineras.registros[listaGasolineras.contador-1].precio << endl;
 
+                    //Damos al usuario la opcion de acceder a la localizacion en Google Maps
                     accederLocalizacion(listaGasolineras, listaGasolineras.contador - 1);
                 }
                 else {
@@ -137,7 +155,7 @@ void menu(tListaGasolineras &listaGasolineras) {
     } while (op != 0);
 }
 
-
+//Metodo para acceder a la localizacion de la gasolinera en GoogleMaps
 void accederLocalizacion(tListaGasolineras listaGasolineras, int posicion) {
     int confirmacion;
     cout << "Desea ver la localizacion en GoogleMaps?" << endl;
@@ -155,6 +173,7 @@ void accederLocalizacion(tListaGasolineras listaGasolineras, int posicion) {
     }
 }
 
+//Metodo para mostrar el menu
 int mostrarMenu() {
     int op = 0;
     cout << "1. Cargar listado de gasolineras en base a tipo de gasolina" << endl;
@@ -172,9 +191,10 @@ int mostrarMenu() {
     return op;
 }
 
+//Metodo para cargar los datos en base al tipo de gasolina escogido
 void cargarDatosGasolineras(tListaGasolineras& listaGasolineras, bool &ok) {
     
-    //Usando CURL
+    //CURL
     
     CURL* curl;
     CURLcode res;
@@ -189,16 +209,17 @@ void cargarDatosGasolineras(tListaGasolineras& listaGasolineras, bool &ok) {
     }
     curl_easy_cleanup(curl);
     Json::StreamWriterBuilder builder;
-    Json::Value json = ParseJson(readBuffer);
-    string output = Json::writeString(builder, json);
+    Json::Value json = ParseJson(readBuffer); //Json cargado
 
+    //Inicializamos la lista
     tGasolineras gasolinera;
     listaGasolineras.contador = 0;
     listaGasolineras.registros = new tGasolineras[N];
     listaGasolineras.DimAct = N;
 
-    const Json::Value& features = json["features"]; // ArrayOfFeatures
+    const Json::Value& features = json["features"]; // Array de la seccion con los datos de las gasolineras
 
+    //Cargamos los datos de las gasolineras en base al tipo de gasolina
     int i = 0;
     while (i < features.size()) {
         gasolinera.localidad = features[i]["attributes"]["localidad"].asString();
@@ -206,9 +227,11 @@ void cargarDatosGasolineras(tListaGasolineras& listaGasolineras, bool &ok) {
         gasolinera.precio = features[i]["attributes"][listaGasolineras.tipoGasolina].asDouble();
         gasolinera.latitud = features[i]["attributes"]["latitud"].asDouble();
         gasolinera.longitud = features[i]["attributes"]["longitud"].asDouble();
+        //Si la lista esta llena pero quedan elementos por procesar, redimensionamos la lista para poder seguir procesandolos.
         if (listaGasolineras.contador == listaGasolineras.DimAct) {
             redimensionar(listaGasolineras);
         }
+        //Filtramos los valores nulos y solo almacenamos los datos validos
         if (features[i]["attributes"][listaGasolineras.tipoGasolina].asDouble() != 0) {
             listaGasolineras.registros[listaGasolineras.contador] = gasolinera;
             listaGasolineras.contador++;
@@ -216,9 +239,11 @@ void cargarDatosGasolineras(tListaGasolineras& listaGasolineras, bool &ok) {
         i++;
     }
 
+    //Confirmamos que los datos han sido cargados
     ok = true;
 }
 
+//Metodo para mostrar todo el listado de gasolineras en base al tipo de gasolina cargado previamente
 void mostrarGasolineras(tListaGasolineras lista) {
     for (int i = 0; i < lista.contador; i++) {
         cout << "Localidad: " << lista.registros[i].localidad << endl;
@@ -230,7 +255,7 @@ void mostrarGasolineras(tListaGasolineras lista) {
     }
 }
 
-
+//Metodo para redimensionar la lista
 void redimensionar(tListaGasolineras &listaGasolineras) {
     tListaGasolineras listaAuxiliar;
     listaGasolineras.DimAct += N;
@@ -244,6 +269,7 @@ void redimensionar(tListaGasolineras &listaGasolineras) {
     delete[] listaAuxiliar.registros;
 }
 
+//Metodo para liberar la memoria una vez ya no la vamos a utilizar
 void liberar(tListaGasolineras& listaGasolineras) {
     delete[] listaGasolineras.registros;
     listaGasolineras.contador = 0;
@@ -274,6 +300,7 @@ static Json::Value ParseJson(string text) {
     return root;
 }
 
+//Metodo para dividir la lista en sublistas
 void merge_sort(tListaGasolineras& gasolineras, int low, int high) {
     int mid;
     if (low < high) {
@@ -286,6 +313,7 @@ void merge_sort(tListaGasolineras& gasolineras, int low, int high) {
     }
 }
 
+//Metodo para ordenar las sublistas y obtener la lista final ordenada de menor a mayor
 void merge(tListaGasolineras& gasolineras, int low, int high, int mid) {
     int i, j, k;
     tGasolineras c[1000];
@@ -319,7 +347,7 @@ void merge(tListaGasolineras& gasolineras, int low, int high, int mid) {
     }
 }
 
-//Sobreescribimos el operador < para que la comparacion sea en base a los precios
+//Metodo para sobrescribir el operador < para que la comparacion en el merge sea en base al precio de la gasolina.
 bool operator<(tGasolineras op1, tGasolineras op2) {
     return op1.precio < op2.precio;
 }
